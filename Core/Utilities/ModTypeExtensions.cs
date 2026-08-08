e98c9405266a29852e12645c6fffd4f7d0eaa645
Blinkkus <168491516+Blinkkus@users.noreply.github.com>
2026-08-07 21:08:24 -0400

    Delete Core directory
    1.1.2

--- Core/Utilities/ModTypeExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NeviliscoVanity.Core.Utilities;
-
-public partial class Utilities
-{
-    public static string GetAssetPath(string prefix, string name) =>
-        $"NeviliscoVanity/Assets/Textures/{prefix}/{name}";
-}
