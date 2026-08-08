2238ada8019943ecee1478ee8cf7fb50e38242cb
Blinkkus <168491516+Blinkkus@users.noreply.github.com>
2026-08-07 21:08:02 -0400

    Delete Content directory
    1.1.2

--- Content/Rarities/NevilsicoRarity.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Xna.Framework;
-using Terraria.ModLoader;
-
-namespace NeviliscoVanity.Content.Rarities;
-
-public class NevilsicoRarity : ModRarity
-{
-    public override Color RarityColor => new Color(37, 150, 190);
-}
