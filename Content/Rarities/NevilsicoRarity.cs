4fe49e41e51669a26589bdf48adf628fa4f79ff1
Blinkkus <168491516+Blinkkus@users.noreply.github.com>
2026-08-15 22:27:37 -0400

    1.1.3 rarity update

--- Content/Rarities/NevilsicoRarity.cs
+++ Content/Rarities/NevilsicoRarity.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace NeviliscoVanity.Content.Rarities;
 
 public class NevilsicoRarity : ModRarity
 {
-    public override Color RarityColor => new Color(37, 150, 190);
+    public override Color RarityColor => Color.Lerp(Color.Black, Color.DarkRed, Main.masterColor);
 }
