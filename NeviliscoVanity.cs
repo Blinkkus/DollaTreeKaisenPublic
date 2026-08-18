d9c200a6525ea03c04e26d232d2a9ebce93d38be
Blinkkus <168491516+Blinkkus@users.noreply.github.com>
2026-08-18 01:29:24 -0400

    hahahahahahaha

--- NeviliscoVanity.cs
+++ NeviliscoVanity.cs
@@ -3,6 +3,7 @@ using Terraria.ModLoader;
 
 namespace NeviliscoVanity
 {
+	// I may or may not be an idiot and typed "Nevil-isco" when making the mod instead of "Nevil-sico"
 	public class NeviliscoVanity : Mod
 	{
 		// Use this to help with "public override void Load()" if used
